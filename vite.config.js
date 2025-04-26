f9bcb8f5516fc29d38b0543d9fb44ee78c03005f
zhd <marwane.zhid@gmail.com>
2025-04-26 23:00:05 +0200

    [add] github page

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
+    base: '/ton-repo/',
     plugins: [svelte()],
     server: {
         historyApiFallback: true, // Permet de rediriger toutes les routes vers l'index
     },
 });
+
+
