7d860307e95364393d10e01b610905056b1ae18a
zhd-lab <marwane.zhid@gmail.com>
2025-04-27 00:26:43 +0200

    [update]

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-    base: '/ton-repo/',
-    plugins: [svelte()],
-    server: {
-        historyApiFallback: true, // Permet de rediriger toutes les routes vers l'index
-    },
-});
-
-
+  base: '/universal-encryption/', // <<< important
+  plugins: [svelte()]
+})
